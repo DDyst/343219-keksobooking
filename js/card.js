6515e70acef5b7749e518996fd1139f64f8d5daf
Kirill <krest_craft@mail.ru>
2017-09-03 19:47:54 +0300

    Доработал инкапсуляцию модулей

---
+++ js/card.js
@@ -0,0 +1,65 @@
+// Модуль для отрисовки панели элемента
+
+'use strict';
+
+(function () {
+  // Объект с соотношением типа апартаментов и соответствующей ему строки
+  var lodgeTypesRelations = {
+    FLAT: 'Квартира',
+    HOUSE: 'Дом',
+    BUNGALO: 'Бунгало'
+  };
+
+  var fragment = document.createDocumentFragment();
+  var panelTemplate = document.getElementById('lodge-template').content;
+  var dialogBlock = document.getElementById('offer-dialog');
+
+  // Функция, переводящая тип апартаментов в удобочитаемый вид
+  var getProperLodgeType = function (type) {
+    return lodgeTypesRelations[type.toUpperCase()];
+  };
+
+  // Функция для преобразования строк из массива в пустые спаны с соответствующим классом
+  var parseFeatures = function (array) {
+    array.forEach(function (item) {
+      var featureElement = document.createElement('span');
+      featureElement.className = 'feature__image feature__image--' + item;
+      fragment.appendChild(featureElement);
+    });
+    return fragment;
+  };
+
+  dialogBlock.classList.add('hidden');
+
+  window.card = {
+    // Функция отрисовки диалоговой панели объявления
+    renderDialogPanel: function (advertisementItem) {
+      var panelElement = panelTemplate.querySelector('.dialog__panel').cloneNode(true);
+      panelElement.querySelector('.lodge__title').textContent = advertisementItem.offer.title;
+      panelElement.querySelector('.lodge__address').textContent = advertisementItem.offer.address;
+      panelElement.querySelector('.lodge__price').innerHTML = advertisementItem.offer.price + '&#x20bd;/ночь';
+      panelElement.querySelector('.lodge__type').textContent = getProperLodgeType(advertisementItem.offer.type);
+      panelElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + advertisementItem.offer.guests + ' гостей в ' + advertisementItem.offer.rooms + ' комнатах';
+      panelElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + advertisementItem.offer.checkin + ', выезд до ' + advertisementItem.offer.checkout;
+      panelElement.querySelector('.lodge__features').appendChild(parseFeatures(advertisementItem.offer.features));
+      panelElement.querySelector('.lodge__description').textContent = advertisementItem.offer.description;
+      dialogBlock.replaceChild(panelElement, dialogBlock.querySelector('.dialog__panel'));
+      dialogBlock.querySelector('.dialog__title img').src = advertisementItem.author.avatar;
+    },
+
+    // Функция, определяющая соответствие между меткой объявления и элементом массива, и отрисовывающая соответствующую диалоговую панель
+    renderProperPanel: function (pinElement) {
+      for (var i = 0; i < window.data.advertisements.length; i++) {
+        if (pinElement.firstChild.getAttribute('src') === window.data.advertisements[i].author.avatar) {
+          this.renderDialogPanel(window.data.advertisements[i]);
+          break;
+        }
+      }
+    },
+
+    // Функция закрытия диалоговой панели объявления
+    closePanel: function () {
+      dialogBlock.classList.add('hidden');
+    }
+  };
+})();
--- js/map.js
+++ js/map.js
@@ -1,95 +1,12 @@
-// Файл для работы с объявлениями на карте
+/*
+Основной модуль
+
+card.js - отрисовка панели объявления
+pin.js - отрисовка меток объявлений и взаимодействие с ними
+*/
 
 'use strict';
 
 (function () {
-  var map = document.querySelector('.tokyo__pin-map');
-  var dialogBlock = document.getElementById('offer-dialog');
-  var dialogClose = dialogBlock.querySelector('.dialog__close');
-  var activePin = false;
 
-  // Функция, снимающая выделение с метки объявления
-  var deactivatePin = function () {
-    activePin.classList.remove('pin--active');
-    activePin = false;
-  };
-
-  // Функция закрытия диалоговой панели объявления
-  var closePanel = function () {
-    dialogBlock.classList.add('hidden');
-  };
-
-  // Функция, определяющая соответствие между меткой объявления и элементом массива, и отрисовывающая соответствующую диалоговую панель
-  var renderProperPanel = function (pinElement) {
-    for (var i = 0; i < window.data.advertisements.length; i++) {
-      if (pinElement.firstChild.getAttribute('src') === window.data.advertisements[i].author.avatar) {
-        window.renderDialogPanel(window.data.advertisements[i]);
-        break;
-      }
-    }
-  };
-
-  // Функция, переключающая активную метку объявления, при условии, что выбранная метка уже не является активной
-  var switchPin = function (pinElement) {
-    if (!activePin || activePin !== pinElement) {
-      if (activePin) {
-        activePin.classList.remove('pin--active');
-      } else {
-        dialogBlock.classList.remove('hidden');
-      }
-      pinElement.classList.add('pin--active');
-      activePin = pinElement;
-    }
-  };
-
-  // Функция, подсвечивающая активируемую метку объявления и открывающая соответствующую ей диалоговую панель
-  var activatePinAndPanel = function (target) {
-    while (target !== map) {
-      if (target.classList.contains('pin') && !target.classList.contains('pin__main')) {
-        switchPin(target);
-        renderProperPanel(target);
-        break;
-      }
-      target = target.parentNode;
-    }
-  };
-
-  // Обработчики событий
-  var mapClickHandler = function (evt) {
-    activatePinAndPanel(evt.target);
-  };
-
-  var mapKeyDownHandler = function (evt) {
-    if (window.util.isEnterPressed(evt.keyCode)) {
-      activatePinAndPanel(evt.target);
-    }
-  };
-
-  var dialogCloseClickHandler = function () {
-    closePanel();
-    deactivatePin();
-  };
-
-  var dialogCloseKeyDownHandler = function (evt) {
-    if (window.util.isEnterPressed(evt.keyCode)) {
-      closePanel();
-      deactivatePin();
-    }
-  };
-
-  var keyDownHandler = function (evt) {
-    if (window.util.isEscPressed(evt.keyCode) && !dialogBlock.classList.contains('hidden')) {
-      closePanel();
-      deactivatePin();
-    }
-  };
-
-  dialogBlock.classList.add('hidden');
-
-  // Вешаем обработчики на метки объявлений и диалоговую панель
-  map.addEventListener('click', mapClickHandler);
-  map.addEventListener('keydown', mapKeyDownHandler);
-  dialogClose.addEventListener('click', dialogCloseClickHandler);
-  dialogClose.addEventListener('keydown', dialogCloseKeyDownHandler);
-  document.addEventListener('keydown', keyDownHandler);
 })();
